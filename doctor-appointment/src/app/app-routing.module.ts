c32cf14cd6ea9c3f71ef23d0bf2f1333965b694a
WangTuann <qngtuann@gmail.com>
2022-05-18 19:49:55 +0700

    Tuan commit page

--- doctor-appointment/src/app/app-routing.module.ts
+++ doctor-appointment/src/app/app-routing.module.ts
@@ -5,23 +5,29 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'login-form',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./pages/folder/folder.module').then(m => m.FolderPageModule)
+    loadChildren: () =>
+      import('./pages/folder/folder.module').then((m) => m.FolderPageModule),
   },
   {
     path: 'login-form',
-    loadChildren: () => import('./pages/login-form/login-form.module').then(m => m.LoginFormPageModule)
+    loadChildren: () =>
+      import('./pages/login-form/login-form.module').then(
+        (m) => m.LoginFormPageModule
+      ),
   },
   {
     path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then((m) => m.SignupPageModule),
   },
   {
     path: 'forgot',
-    loadChildren: () => import('./pages/forgot/forgot.module').then(m => m.ForgotPageModule)
+    loadChildren: () =>
+      import('./pages/forgot/forgot.module').then((m) => m.ForgotPageModule),
   },
   // {
   //   path: 'location',
@@ -42,35 +48,55 @@ const routes: Routes = [
   // },
   {
     path: 'cardiologist',
-    loadChildren: () => import('./pages/cardiologist/cardiologist.module').then(m => m.CardiologistPageModule)
+    loadChildren: () =>
+      import('./pages/cardiologist/cardiologist.module').then(
+        (m) => m.CardiologistPageModule
+      ),
   },
   {
     path: 'map-doctor',
-    loadChildren: () => import('./pages/map/map.module').then(m => m.MapPageModule)
+    loadChildren: () =>
+      import('./pages/map/map.module').then((m) => m.MapPageModule),
   },
   {
     path: 'book-appointment',
-    loadChildren: () => import('./pages/book-appointment/book-appointment.module').then(m => m.BookAppointmentPageModule)
+    loadChildren: () =>
+      import('./pages/book-appointment/book-appointment.module').then(
+        (m) => m.BookAppointmentPageModule
+      ),
   },
   {
-    path: 'doctor-info/:id',
-    loadChildren: () => import('./pages/doctor-info/doctor-info.module').then(m => m.DoctorInfoPageModule)
+    path: 'doctor-info', //xoa id
+    loadChildren: () =>
+      import('./pages/doctor-info/doctor-info.module').then(
+        (m) => m.DoctorInfoPageModule
+      ),
   },
   {
     path: 'my-appointment',
-    loadChildren: () => import('./pages/my-appointment/my-appointment.module').then(m => m.MyAppointmentPageModule)
+    loadChildren: () =>
+      import('./pages/my-appointment/my-appointment.module').then(
+        (m) => m.MyAppointmentPageModule
+      ),
   },
   {
     path: 'appointment-detail/:id',
-    loadChildren: () => import('./pages/appointment-detail/appointment-detail.module').then(m => m.AppointmentDetailPageModule)
+    loadChildren: () =>
+      import('./pages/appointment-detail/appointment-detail.module').then(
+        (m) => m.AppointmentDetailPageModule
+      ),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
   {
     path: 'appointment-schedule/:id',
-    loadChildren: () => import('./pages/appointment-schedule/appointment-schedule.module').then(m => m.AppointmentSchedulePageModule)
+    loadChildren: () =>
+      import('./pages/appointment-schedule/appointment-schedule.module').then(
+        (m) => m.AppointmentSchedulePageModule
+      ),
   },
   // {
   //   path: 'lab-test',
@@ -82,50 +108,70 @@ const routes: Routes = [
   // },
   {
     path: 'search-test',
-    loadChildren: () => import('./pages/search-test/search-test.module').then(m => m.SearchTestPageModule)
+    loadChildren: () =>
+      import('./pages/search-test/search-test.module').then(
+        (m) => m.SearchTestPageModule
+      ),
   },
   {
     path: 'find-doctor',
-    loadChildren: () => import('./pages/medical-shop/medical-shop.module').then(m => m.MedicalShopPageModule)
+    loadChildren: () =>
+      import('./pages/medical-shop/medical-shop.module').then(
+        (m) => m.MedicalShopPageModule
+      ),
   },
   {
     path: 'lab-map',
-    loadChildren: () => import('./pages/lab-map/lab-map.module').then(m => m.LabMapPageModule)
+    loadChildren: () =>
+      import('./pages/lab-map/lab-map.module').then((m) => m.LabMapPageModule),
   },
   {
     path: 'chat',
-    loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatPageModule)
+    loadChildren: () =>
+      import('./pages/chat/chat.module').then((m) => m.ChatPageModule),
   },
   {
     path: 'chating',
-    loadChildren: () => import('./pages/chating/chating.module').then(m => m.ChatingPageModule)
+    loadChildren: () =>
+      import('./pages/chating/chating.module').then((m) => m.ChatingPageModule),
   },
   {
     path: 'feedback',
-    loadChildren: () => import('./pages/feedback/feedback.module').then(m => m.FeedbackPageModule)
+    loadChildren: () =>
+      import('./pages/feedback/feedback.module').then(
+        (m) => m.FeedbackPageModule
+      ),
   },
   {
     path: 'homepage',
-    loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepagePageModule)
+    loadChildren: () =>
+      import('./pages/homepage/homepage.module').then(
+        (m) => m.HomepagePageModule
+      ),
   },
   {
     path: 'specialities',
-    loadChildren: () => import('./pages/specialities/specialities.module').then(m => m.SpecialitiesPageModule)
+    loadChildren: () =>
+      import('./pages/specialities/specialities.module').then(
+        (m) => m.SpecialitiesPageModule
+      ),
   },
   {
     path: 'user-signup',
-    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
+    loadChildren: () =>
+      import('./pages/user/user.module').then((m) => m.UserPageModule),
   },
   {
     path: 'doctors-signup',
-    loadChildren: () => import('./pages/doctors/doctors.module').then( m => m.DoctorsPageModule)
-  }
+    loadChildren: () =>
+      import('./pages/doctors/doctors.module').then((m) => m.DoctorsPageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
