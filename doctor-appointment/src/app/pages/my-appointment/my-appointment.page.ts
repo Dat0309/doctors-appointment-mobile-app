d3a1d3fa608910e6b34479d473fe2f461b3220e8
Dat0309 <anhtheboy011@gmail.com>
2022-05-24 19:57:02 +0700

    Update

--- doctor-appointment/src/app/pages/my-appointment/my-appointment.page.ts
+++ doctor-appointment/src/app/pages/my-appointment/my-appointment.page.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Appointment, AppointmentService } from 'src/app/services/appointment/appointment.service';
+import { Company } from 'src/app/services/company/company.service';
 import { Doctor, DoctorService } from 'src/app/services/doctor/doctor.service';
 
 @Component({
@@ -18,17 +19,19 @@ export class MyAppointmentPage implements OnInit {
     ) { }
 
     appointments$: Appointment[] = [];
-    doctors$: Doctor[] = [];
+    doctor$: Doctor;
+    company$: Company[] = [];
 
   ngOnInit() {
     this.getMyAppointment('62793c38fe74f2163ea2920a');
+    this.getDoctorByID('627939dbdf860bc50f9ad04e');
   }
 
   public getMyAppointment(id: string) {
     this.appointmentService.getAllByCustomerId(id).subscribe(
       (res: any) => {
         if (res != null) {
-          this.appointments$ = res;
+          this.appointments$ = res.appointment;
           console.log(this.appointments$);
         } else {
           console.log('Fail to load appoinment');
@@ -41,8 +44,8 @@ export class MyAppointmentPage implements OnInit {
     this.doctorService.getByID(id).subscribe(
       (res: any)=>{
         if(res != null) {
-          this.doctors$ = res;
-          console.log(this.doctors$);
+          this.doctor$ = res;
+          console.log(this.doctor$);
         }else{
           console.log('Failed to load doctor');
         }
