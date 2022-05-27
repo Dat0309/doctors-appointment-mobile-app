cbb46979da79b822d7dd1c23c350ae5381909fad
GoMinn_1604 <90507635+gominn1604@users.noreply.github.com>
2022-05-27 18:20:49 +0700

    commit

--- doctor-appointment/src/app/pages/lab-map/lab-map.page.ts
+++ doctor-appointment/src/app/pages/lab-map/lab-map.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabMapPage implements OnInit {
 
+  longitudeEvent: string;
+  latitudeEvent: string;
   constructor() { }
 
   ngOnInit() {
